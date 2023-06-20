5c8e687844fcb13ef7539dd8e6b97ce78370b12a
Nick Van Reet <nick.vanreet@gmail.com>
2023-06-20 19:22:28 +0200

    also rename file & headers

--- cleanup_FASTA_headers.py
+++ cleanup_FASTA_headers.py
@@ -4,6 +4,7 @@ from Bio import SeqIO
 
 def shorten_fasta_headers(input_dir):
     fasta_files = [filename for filename in os.listdir(input_dir) if filename.endswith(".fasta")]
+    directory_name = os.path.basename(os.path.normpath(input_dir)) # Get the last folder name
 
     for filename in fasta_files:
         fasta_file = os.path.join(input_dir, filename)
@@ -12,12 +13,18 @@ def shorten_fasta_headers(input_dir):
         sequences = SeqIO.parse(fasta_file, "fasta")
         shortened_sequences = []
         for seq in sequences:
-            seq.id = seq.id.split()[0]  # Modify this line based on your needs
+            seq.id = directory_name + "_" + seq.id.split()[0]  # Modify this line based on your needs
             seq.description = ''  # Clear the description to prevent it from being added to the header
             shortened_sequences.append(seq)
         
+        # Create the new filename with the directory_name as prefix
+        new_filename = directory_name + "_" + filename
+        new_filepath = os.path.join(input_dir, new_filename)
+        
         # Overwrite the original file with the sequences having shortened headers
-        SeqIO.write(shortened_sequences, fasta_file, "fasta")
+        SeqIO.write(shortened_sequences, new_filepath, "fasta")
+        # Remove the original fasta file
+        os.remove(fasta_file)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Shorten FASTA headers.')
@@ -25,3 +32,4 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     shorten_fasta_headers(args.input_dir)
+

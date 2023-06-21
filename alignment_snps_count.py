ccf33a2695a1cfc40b7a25b099d382c747aa42b2
Nick Van Reet <nick.vanreet@gmail.com>
2023-06-21 20:48:31 +0200

    fucking awesome results

--- alignment_snps_count.py
+++ alignment_snps_count.py
@@ -1,6 +1,8 @@
 from Bio import SeqIO
 from collections import defaultdict
-import os
+import pandas as pd
+
+possible_nucleotides = ['A', 'T', 'C', 'G', 'R', 'Y', 'S', 'W', 'K', 'M', 'B', 'D', 'H', 'V', 'N', '-']
 
 def parse_sequence_file(file_path):
     with open(file_path, "r") as handle:
@@ -8,9 +10,7 @@ def parse_sequence_file(file_path):
     return sequences
 
 def find_tbg_specific_snps(sequences):
-    snp_positions = defaultdict(lambda: defaultdict(int))
-    tbg_specific_snps = defaultdict(list)
-    possible_nucleotides = ['A', 'T', 'C', 'G', 'R', 'Y', 'S', 'W', 'K', 'M', 'B', 'D', 'H', 'V', 'N', '-']
+    snp_positions = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
 
     for seq_record in sequences:
         description_fields = seq_record.description.split('|')
@@ -23,23 +23,27 @@ def find_tbg_specific_snps(sequences):
             hits = [hit.strip() for hit in hits_field.split(':')[1].split('&')]
             for i, nucleotide in enumerate(str(seq_record.seq)):
                 for hit in hits:
-                    snp_positions[i+1][hit.split('_')[0] + '_' + nucleotide] += 1
+                    hit_parts = hit.split('_')
+                    if len(hit_parts) >= 2:
+                        species, strain = hit_parts[:2]
+                    else:
+                        species = hit_parts[0]
+                        strain = 'unknown'
+                    snp_positions[i+1][(species, strain)][nucleotide] += 1
 
-    for position, snps in snp_positions.items():
-        for possible_nucleotide in possible_nucleotides:
-            tbg_count = sum(count for snp, count in snps.items() if 'Tbg_' + possible_nucleotide in snp)
-            tbb_tbr_count = sum(count for snp, count in snps.items() if 'Tbb_' + possible_nucleotide in snp or 'Tbr_' + possible_nucleotide in snp)
-            tbg_specific_snps[position].append((possible_nucleotide, tbg_count, tbb_tbr_count))
-
-    return tbg_specific_snps
+    return snp_positions
 
 def write_to_file(tbg_specific_snps, output_file):
-    with open(output_file, 'w') as file:
-        file.write('Position\tSNP in Tbg\tTotal Tbg Clones\tSNP in Tbb/Tbr\tTotal Tbb/Tbr Clones\n')
-        for position, snps in tbg_specific_snps.items():
-            for snp in snps:
-                file.write(f'{position}\tTbg_{snp[0]}\t{snp[1]}\tTbb/Tbr_{snp[0]}\t{snp[2]}\n')
+    data = []
+    for position, species_strains in tbg_specific_snps.items():
+        for species_strain, snps in species_strains.items():
+            for nucleotide, count in snps.items():
+                data.append([position, nucleotide, '_'.join(species_strain), count])
+
+    df = pd.DataFrame(data, columns=['Position', 'Nucleotide', 'Species_Strain', 'Count'])
+    df = df.pivot_table(index=['Position', 'Nucleotide'], columns='Species_Strain', values='Count', fill_value=0)
+    df.to_csv(output_file)
 
 sequences = parse_sequence_file('unique_sequences_175_180bp_GCGCAGTT.fa')
 tbg_specific_snps = find_tbg_specific_snps(sequences)
-write_to_file(tbg_specific_snps, 'SNP_counts.txt')
+write_to_file(tbg_specific_snps, 'SNP_counts.csv')
